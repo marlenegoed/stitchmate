2df6b1ed72075e662e294a75665269ebe5a6a03d
Daniel Ort <md.ort@gmx.de>
2024-04-26 17:46:13 +0200

    added fonts.ts

---
+++ src/components/ui/fonts.ts
@@ -0,0 +1,12 @@
+import {Just_Another_Hand} from 'next/font/google';
+import {Lato} from 'next/font/google';
+
+export const justAnotherHand = Just_Another_Hand({
+    subsets: ['latin'],
+     weight: ['400']
+    });
+
+export const lato = Lato({
+    subsets: ['latin'], 
+    weight: ['100', '300', '400', '700']
+    });
