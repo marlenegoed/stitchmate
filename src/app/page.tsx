627cbe4464373c7586b2009924332d15a6e1cda6
Marlene <marlene.goedecke@gmail.com>
2024-05-24 18:02:31 +0200

    Refactor demo page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,8 @@
+import {redirect} from 'next/navigation'
+import {auth} from "@clerk/nextjs/server";
+
 export default async function Page() {
+  if (auth().userId) redirect('/projects')
+
   return "Landing page"
 }
