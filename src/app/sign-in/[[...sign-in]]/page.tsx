e444d5408d56bfced77e92a21e64272b65c9f64d
Marlene <marlene.goedecke@gmail.com>
2024-05-24 17:03:54 +0200

    Add background to sign up page

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -4,7 +4,6 @@ import landingImg from '@/../public/sm_landing_blobs.svg'
 export default function Page() {
   return (
     <main className="flex items-center justify-center h-full bg-sienna-100/50 bg-no-repeat bg-cover" style={{backgroundImage: `url(${landingImg.src})`}}>
-
       <SignIn path="/sign-in" />
     </main>
   )
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -1,8 +1,9 @@
 import {SignUp} from "@clerk/nextjs";
+import landingImg from '@/../public/sm_landing_blobs.svg'
 
 export default function Page() {
   return (
-    <main className="flex items-center justify-center h-full">
+    <main className="flex items-center justify-center h-full bg-sienna-100/50 bg-no-repeat bg-cover" style={{backgroundImage: `url(${landingImg.src})`}}>
       <SignUp path="/sign-up" />;
     </main>
   )
