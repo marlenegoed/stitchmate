b8c069d44ebc79a14fd5ec0882aee971846d42c0
Marlene <marlene.goedecke@gmail.com>
2024-05-24 17:02:23 +0200

    Move demo counter to demo page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,3 @@
-import {redirect} from 'next/navigation'
-import {DemoCounterPage} from './demo-counter-page'
-import {auth} from "@clerk/nextjs/server";
-import Nav from '@/components/ui/navbar';
-import {CounterStoreProvider} from '@/providers/counter-store-provider';
-import {DemoStoreProvider} from '@/providers/demo-store-provider';
-import {UserSettingsStoreProvider} from '@/providers/user-settings-store-provider';
-
-export default function Page() {
-  if (auth().userId) redirect('/projects')
-
-  return (
-    <>
-      <Nav />
-      <CounterStoreProvider>
-        <UserSettingsStoreProvider>
-          <DemoStoreProvider>
-            <main className='h-[calc(100%-4rem)] flex flex-col mx-auto items-center py-3 w-full bg-neutral-100 rounded-t-2xl shadow'>
-              <DemoCounterPage />
-            </main>
-          </DemoStoreProvider>
-        </UserSettingsStoreProvider>
-      </CounterStoreProvider >
-    </>
-  )
+export default async function Page() {
+  return "Landing page"
 }
