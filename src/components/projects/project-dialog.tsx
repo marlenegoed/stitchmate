369a6d23cc659f1bbd14eec05dacbb0b060fc9a7
Marlene <marlene.goedecke@gmail.com>
2024-05-29 08:36:19 +0200

    project page: added drawer for mobile view

--- src/components/projects/project-dialog.tsx
+++ src/components/projects/project-dialog.tsx
@@ -8,6 +8,18 @@ import {
   DialogTitle,
   DialogTrigger
 } from "@/components/ui/dialog";
+
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
+
 import {HiOutlineSquaresPlus} from "react-icons/hi2";
 
 import {HiChevronRight} from "react-icons/hi";
@@ -31,6 +43,7 @@ import {createNewProject, quickStartProject} from '@/database/queries/projects';
 import {useState} from 'react';
 import generateBlobId from '@/lib/generate-blob-id';
 import {cn} from '@/lib/utils';
+import {useMediaQuery} from '@/lib/use-media-query';
 
 
 const formSchema = z.object({
@@ -44,12 +57,10 @@ const formSchema = z.object({
 
 export default function ProjectDialog({userId, className}: {userId: string, className?: string}) {
   const [open, setOpen] = useState(false)
+  const isDesktop = useMediaQuery("(min-width: 1024px)")
 
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: {
-      title: 'Project Title',
-    }
   })
 
 
@@ -70,49 +81,103 @@ export default function ProjectDialog({userId, className}: {userId: string, clas
     setOpen(false)
   }
 
+  if (isDesktop) {
+
+    return (
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button type='button' size='icon' variant='ghost' className={cn('text-sienna-400 hover:bg-neutral-200', className)}>
+            <HiOutlineSquaresPlus size={22} />
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px] p-10">
+          <DialogHeader className='mb-2 -mt-2'>
+            <DialogTitle className='mb-2 mr-auto font-semibold text-xl'>New Project</DialogTitle>
+          </DialogHeader>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+              <FormField
+                control={form.control}
+                name="title"
+                render={({field}) => (
+                  <FormItem>
+                    <FormLabel>Title</FormLabel>
+                    <FormControl>
+                      <Input variant='form' placeholder='Project Title' {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <DialogFooter>
+                <div className='grid grid-cols-2 gap-4'>
+                  <p onClick={handleClick} className="sm:col-span-1 col-span-2 cursor-pointer underline underline-offset-4 text-slate-700 hover:text-slate-950 transition-colors font-semibold flex justify-self-center sm:justify-self-start	self-center pl-1 sm:order-1 order-2 sm:pt-0 pt-2">More Settings
+                  </p>
+                  <Button type='submit' variant='outline' className='border-sienna-300 text-sienna-300 hover:text-sienna-400/80 hover:border-sienna-400/80 pl-8 pr-6 w-fit sm:col-span-1 col-span-2 justify-self-center sm:justify-self-end sm:mt-0 mt-2 sm:order-2 order-1'>
+                    <span className='flex flex-row items-center gap-2 justify-center'>
+                      Quickstart
+                      <HiChevronRight size={20} className='text-sienna-300' />
+                    </span>
+                  </Button>
+                </div>
+              </DialogFooter>
+            </form>
+          </Form>
+        </DialogContent>
+      </Dialog >
+    );
+
+  }
+
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger asChild>
+
+    <Drawer open={open} onOpenChange={setOpen}>
+      <DrawerTrigger asChild>
         <Button type='button' size='icon' variant='ghost' className={cn('text-sienna-400 hover:bg-neutral-200', className)}>
           <HiOutlineSquaresPlus size={22} />
         </Button>
-      </DialogTrigger>
-      <DialogContent className="sm:max-w-[425px] p-10">
-        <DialogHeader className='mb-2 -mt-2'>
-          <DialogTitle className='mb-2 mr-auto font-semibold text-xl'>New Project</DialogTitle>
-        </DialogHeader>
+      </DrawerTrigger>
+      <DrawerContent>
+
+        <DrawerHeader className="text-center my-6">
+          <DrawerTitle>
+            Create new Project:
+          </DrawerTitle>
+        </DrawerHeader>
+
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+
             <FormField
               control={form.control}
               name="title"
               render={({field}) => (
                 <FormItem>
-                  <FormLabel>Title</FormLabel>
                   <FormControl>
-                    <Input variant='form' placeholder='Project Title' {...field} />
+                    <Input variant='form' placeholder='Add title' {...field} />
                   </FormControl>
                   <FormMessage />
                 </FormItem>
               )}
             />
 
-            <DialogFooter>
+            <DrawerFooter className="pt-2">
               <div className='grid grid-cols-2 gap-4'>
-                <p onClick={handleClick} className="sm:col-span-1 col-span-2 cursor-pointer underline underline-offset-4 text-slate-700 hover:text-slate-950 transition-colors font-semibold flex justify-self-center sm:justify-self-start	self-center pl-1 sm:order-1 order-2 sm:pt-0 pt-2">More Settings
+                <p onClick={handleClick} className="sm:col-span-1 col-span-2 cursor-pointer underline underline-offset-4 text-neutral-400 hover:text-neutral-500 transition-colors font-semibold flex justify-self-center sm:justify-self-start	self-center pl-1 sm:order-1 order-2 sm:pt-0 pt-2">More Settings
                 </p>
-                <Button type='submit' variant='outline' className='border-sienna-300 text-sienna-300 hover:text-sienna-400/80 hover:border-sienna-400/80 pl-8 pr-6 w-fit sm:col-span-1 col-span-2 justify-self-center sm:justify-self-end sm:mt-0 mt-2 sm:order-2 order-1'>
+                <Button type='submit' variant='outline' className='border-sienna-300 text-sienna-300 hover:text-sienna-400/80 hover:border-sienna-400/80 pl-8 pr-6 w-full sm:col-span-1 col-span-2 justify-self-center sm:justify-self-end sm:mt-0 mt-2 sm:order-2 order-1'>
                   <span className='flex flex-row items-center gap-2 justify-center'>
                     Quickstart
                     <HiChevronRight size={20} className='text-sienna-300' />
                   </span>
                 </Button>
               </div>
-            </DialogFooter>
+            </DrawerFooter>
           </form>
         </Form>
-      </DialogContent>
-    </Dialog >
-  );
+      </DrawerContent>
+    </Drawer >
+  )
 }
 
