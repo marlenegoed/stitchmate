9389ecd84c7edffbaaf52e04ce39cf347ddca0ef
Marlene <marlene.goedecke@gmail.com>
2024-05-29 13:50:05 +0200

    added missing files

---
+++ src/components/ui/beta-banner.tsx
@@ -0,0 +1,9 @@
+export default function BetaBanner() {
+
+  return (
+    <span className='h-6 w-32 bg-white/80 shadow -rotate-45 text-center fixed -left-8 top-4'>
+      <p className='font-semibold text-sienna-400 text-center'>beta
+      </p>
+    </span>
+  )
+}
