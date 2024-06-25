de7210d12925ed1fff427a48e02a063ac713b822
Marlene <marlene.goedecke@gmail.com>
2024-06-25 14:51:15 +0200

    removed flex box from root layout, added layout & nav to sign-in and sign-out page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export default function RootLayout({children}: Readonly<{
   return (
     <ClerkProvider>
       <html lang="en">
-        <body className={`${openSans.className} antialiased h-full min-h-dvh bg-neutral-100 flex justify-center items-center`}>
+        <body className={`${openSans.className} antialiased h-full min-h-dvh bg-neutral-100`}>
           {children}
           <Toaster />
         </body>
