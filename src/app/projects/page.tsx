9b45a64b1d8be93c0496807d3078a75cdc745150
Marlene <marlene.goedecke@gmail.com>
2024-06-26 12:41:58 +0200

    moved stores to session storage

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,21 +1,26 @@
-
+'use server'
 import {Suspense} from 'react';
 import {auth} from "@clerk/nextjs/server";
 import {ProjectsHeader} from '../../components/projects/projects-header';
 import {ProjectList} from '../../components/projects/project-list';
 import Loading from '../loading';
+import Guide from '@/components/ui/guide';
+import HydrateUserSettingsStore from '@/components/store/hydrate-user-settings-store';
+import {getUserSettings} from '@/database/queries/queries';
 
 
 export default async function Page({searchParams}: {searchParams?: {title?: string, favorite?: string, page?: number}}) {
   const {userId} = auth().protect();
-
+  const userSettings = await getUserSettings(userId)
   const title = searchParams?.title || ''
   const favorite = searchParams?.favorite === '1' ? true : false;
   const page = searchParams?.page || 0
 
   return (
     <>
+      <HydrateUserSettingsStore storeSound={userSettings.sound} showGuide={userSettings.guide} />
       <ProjectsHeader userId={userId} />
+      <Guide />
       <Suspense fallback={<Loading />}>
         <ProjectList title={title} favorite={favorite} userId={userId} page={page} />
       </Suspense>
