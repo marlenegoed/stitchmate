c78bf300be3d2f08461ce29689915245c9285865
Marlene <marlene.goedecke@gmail.com>
2024-07-16 10:18:35 +0200

    separeted section pagination to own component

--- src/components/sections/section-header.tsx
+++ src/components/sections/section-header.tsx
@@ -1,14 +1,7 @@
 'use client'
 
-import CounterActions from './couter-actions'
-import {HiChevronLeft} from "react-icons/hi2";
-import {HiChevronRight} from "react-icons/hi2";
-import {Button} from '../ui/button';
-import {Reminder, Section, UserSettings, changeActiveSection} from '@/database/queries/queries';
+import {Section, UserSettings} from '@/database/queries/queries';
 import SectionTitleField from './section-title-field';
-import Title from '../ui/title';
-import shortenText from '@/lib/shorten-text';
-import {Tooltip} from '../ui/tooltip';
 import {cn} from '@/lib/utils';
 import NumOfRows from './num-of-rows';
 import {useMediaQuery} from '@/lib/use-media-query';
@@ -34,113 +27,6 @@ export default function SectionHeader({section, userSettings, className,}: Secti
       {showNumOfRows &&
         <NumOfRows numOfRows={section.numOfRows ? section.numOfRows : 0} />
       }
-    </div>
-  )
-}
-
-
-interface SectionPaginationProps {
-  userId: string,
-  section: Section,
-  numOfSections: number
-}
-
-function SectionPagination({userId, section, numOfSections}: SectionPaginationProps) {
-  // TODO: Make this more flexibel and show current position 
-  async function moveToPrevSection() {
-    if (section.position <= 0) return
-    const newPosition = section.position - 1
-    await changeActiveSection(userId, section.projectId, newPosition)
-  }
-
-  async function moveToNextSection() {
-    const newPosition = section.position + 1
-    if (newPosition >= numOfSections) return
-    await changeActiveSection(userId, section.projectId, newPosition)
-  }
-
-  return (
-    <div className='flex flex-row text-slate-700'>
-      <Tooltip title="Go to previous section">
-        <Button
-          size='icon'
-          variant='ghost'
-          className='disabled:opacity-30 hover:bg-neutral-200 hover:bg-opacity-80 transition-colors'
-          onClick={moveToPrevSection}
-          disabled={section.position <= 0 ? true : false}
-        >
-          <HiChevronLeft size={24} />
-        </Button>
-      </Tooltip>
-
-      <Tooltip title="Go to next section">
-        <Button
-          size='icon'
-          variant='ghost'
-          className='disabled:opacity-30 hover:bg-neutral-200 hover:bg-opacity-80 transition-colors'
-          onClick={moveToNextSection}
-          disabled={section.position + 1 >= numOfSections ? true : false}
-        >
-          <HiChevronRight size={24} />
-        </Button>
-      </Tooltip>
-    </div>
-  )
-}
-
-
-
-interface MoveToSectionProps {
-  userId: string,
-  section: Section,
-  numOfSections: number,
-  className?: string
-}
-
-function MoveToNextSection({userId, section, numOfSections, className}: MoveToSectionProps) {
-
-
-  async function moveToNextSection() {
-    const newPosition = section.position + 1
-    if (newPosition >= numOfSections) return
-    await changeActiveSection(userId, section.projectId, newPosition)
-  }
-
-  return (
-
-    <div className={cn('flex flex-row text-slate-700', className)}>
-      <Button
-        size='icon'
-        variant='ghost'
-        className='disabled:opacity-30 hover:bg-neutral-200 hover:bg-opacity-80 transition-colors'
-        onClick={moveToNextSection}
-        disabled={section.position + 1 >= numOfSections ? true : false}
-      >
-        <HiChevronRight size={24} />
-      </Button>
-    </div>
-  )
-}
-
-
-function MoveToPrevSection({userId, section, className}: MoveToSectionProps) {
-  async function moveToPrevSection() {
-    if (section.position <= 0) return
-    const newPosition = section.position - 1
-    await changeActiveSection(userId, section.projectId, newPosition)
-  }
-
-  return (
-    <div className={cn('flex flex-row text-slate-700', className)}>
-      <Button
-        size='icon'
-        variant='ghost'
-        className='disabled:opacity-30 hover:bg-neutral-200 hover:bg-opacity-80 transition-colors'
-        onClick={moveToPrevSection}
-        disabled={section.position <= 0 ? true : false}
-      >
-        <HiChevronLeft size={24} />
-      </Button>
     </div>
   )
 }
