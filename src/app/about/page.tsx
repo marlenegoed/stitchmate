ab1d9437dc0299352d0829fd264fb5e5c4412799
Marlene <marlene.goedecke@gmail.com>
2024-10-26 19:02:01 +0200

    layout improvements about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,22 +14,24 @@ export default function Page() {
 
   return (
     <div className='text-gray-900 flex flex-col text-left p-6 text-2xl sm:text-3xl h-[calc(100vh_-_4rem)] font-medium max-w-screen-lg mx-auto space-y-10 justify-between'>
-   
-    <section className='space-y-10 flex-1'>
+
+      <section className='space-y-10 flex-1'>
         <p className='leading-relaxed'>I&apos;m Marlene, software developer and enthusiastic knitter. Stitchmate is a personal project that fuses my passion for knitting with my skills as a creative developer.
         </p>
         <p className='leading-relaxed'>For questions and improvements <span><a className='hover:opacity-80 whitespace-nowrap cursor-pointer font-bold' href='mailto:stitchmate.contact@gmail.com'>contact me.</a></span> </p>
 
       </section>
 
-        <section className='grid md:grid-cols-3 md:gap-0 lg:gap-20 sm:pt-10  w-full items-start sm:items-center'>
+      <div className='space-y-10 pb-10'>
+
+        <section className='flex justify-between w-full sm:items-center'>
 
           <div className="sm:text-3xl text-lg flex flex-row items-center gap-2">
             <p className='whitespace-nowrap'>Happy Making</p>
             <HiHeart size={24} className='mt-1 h-8' />
           </div>
 
-          <Button size="lg" variant="outline" className="col-start-3 w-fit border-black hover:bg-black hover:border-black text-black hover:text-white transition hover:transition-all hover:duration-700 duration-700" onClick={() => router.back()}>
+          <Button size="lg" variant="outline" className="col-start-3 w-fit border-black hover:bg-black hover:border-black text-black hover:text-white transition hover:transition-all hover:duration-700 duration-700 rounded-lg" onClick={() => router.back()}>
             go back
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 ml-4">
               <path fillRule="evenodd" d="M16.72 7.72a.75.75 0 0 1 1.06 0l3.75 3.75a.75.75 0 0 1 0 1.06l-3.75 3.75a.75.75 0 1 1-1.06-1.06l2.47-2.47H3a.75.75 0 0 1 0-1.5h16.19l-2.47-2.47a.75.75 0 0 1 0-1.06Z" clipRule="evenodd" />
@@ -40,25 +42,26 @@ export default function Page() {
 
 
 
-        <section className="grid md:grid-cols-3 gap-6 lg:gap-20 border-t pt-10 mt-auto">
+        <section className="grid md:grid-cols-3 gap-8 lg:gap-28 border-t pt-10">
 
-          <p className="text-base sm:text-lg leading-relaxed">
-            Find my source code on <a href="https://github.com/marlenegoed/stitchmate" className="font-semibold whitespace-nowrap">github
+          <p className="text-base/8 sm:text-lg/8">
+            My source code: <a href="https://github.com/marlenegoed/stitchmate" className="font-semibold whitespace-nowrap">github
               <FaGithub className='inline -mt-1 ml-2' size={24} /></a>
           </p>
 
-          <p className="text-base sm:text-lg leading-relaxed">
-            My crafting related social accounts:
+          <p className="text-base/8 sm:text-lg/8 md:justify-self-center">
+            Crafting on social: 
             <a href='https://www.ravelry.com/people/craftystitchess'><FaRavelry size={24} className='inline -mt-1 ml-2' /></a>
             <a href='https://www.instagram.com/crafty_stitchess'><FaInstagram size={24} className='inline whitespace-nowrap -mt-1 ml-2' /></a>
           </p>
 
-          <p className="text-base sm:text-lg">
-            Attributions: <a href="https://www.vecteezy.com/free-vector/blob">Blob Vectors by Vecteezy</a>
+          <p className="text-base/8 sm:text-lg/8 md:justify-self-end">
+            <a href="https://www.vecteezy.com/free-vector/blob">Blob Vectors by Vecteezy</a>
           </p>
 
         </section>
+      </div>
 
-      </div >
+    </div >
   )
 }
